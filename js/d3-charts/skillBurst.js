92b43b7289c7b77e4b36a494fce446a70ce50f7f
nbuechler <natebuechler@gmail.com>
2016-04-08 23:49:21 -0400

    sunburst stuff

---
+++ js/d3-charts/skillBurst.js
@@ -0,0 +1,498 @@
+var root = {
+	"name": "Nathaniel Buechler",
+	"color": "silver",
+		"children": [
+			{
+			"name": "Artist",
+			"color": "#56222E",
+			"children": [
+				{
+				"name": "Artist",
+				"color": "#56222E",
+				"children": [
+					{"name": "Charcoal", "size": 1},
+					{"name": "Oil Pastel", "size": 1},
+					{"name": "Graphite", "size": 1},
+					{"name": "Pencil", "size": 1},
+					]
+				},
+				{
+				"name": "Graphic Designer",
+				"color": "#4D0036",
+				"children": [
+					{
+					"name": "Designing",
+					"children": [
+						{"name": "Books", "size": 1},
+						{"name": "Newsletter", "size": 1},
+						{"name": "Poster", "size": 1},
+						{"name": "Banner", "size": 1},
+						{"name": "Flier", "size": 1},
+						{"name": "Logo", "size": 1},
+						{"name": "Motion Graphics", "size": 1},
+						{"name": "Web Graphics", "size": 1}
+						]
+					},
+					{
+					"name": "Graphic Design Software",
+					"children": [
+						{"name": "GIMP","size": 1},
+						{"name": "Illustrator","size": 1},
+						{"name": "InDesign", "size": 1},
+						{"name": "Photoshop", "size": 1},
+						{"name": "Flash", "size": 1},
+						{"name": "AfterEffects", "size": 1},
+						{"name": "Dreamweaver", "size": 1},
+						{"name": "Bridge", "size": 1},
+						{"name": "Acrobat", "size": 1}
+						]
+					},
+					]
+				},
+				{
+				"name": "Sound Designer",
+				"color": "#7F406B",
+				"children": [
+					{"name": "Audtion","size": 1},
+					{"name": "Logic Pro", "size": 1}
+					]
+				},
+				{
+				"name": "Photographer",
+				"color": "#824648",
+				"children": [
+					{"name": "Photography",
+					"children": [
+						{"name": "Fireworks", "size": 1},
+						{"name": "Landscape", "size": 1},
+						{"name": "Manufactured", "size": 1},
+						{"name": "Nature", "size": 1},
+						{"name": "Single Subject", "size": 1},
+						{"name": "Panorama", "size": 1}
+						]
+					},
+					]
+				},
+				{
+				"name": "Painter",
+				"color": "#6C3433",
+				"children": [
+					{"name": "Watercolor", "size": 1},
+					{"name": "Acrylic", "size": 1},
+					{"name": "Oil", "size": 1}
+					]
+				},
+				{
+				"name": "Musician",
+				"color": "#3B2F23",
+				"children": [
+					{"name": "Trumpet", "size": 1},
+					{"name": "Cello", "size": 1}
+					]
+				}
+			]
+			},
+			{
+			"name": "Computer Programmer",
+			"color": "#632F5F",
+			"children": [
+				{
+				"name": "Basic Programming",
+				"years": 4,
+				"color": "#632F5F",
+				"children": [
+					{"name": "HTML/XHTML/HTML5", "years": 4, "size": 1},
+					{"name": "CSS/CSS2/CSS3", "years": 4, "size": 1},
+					{"name": "JavaScript", "years": 3, "size": 1},
+					]
+				},
+				{
+				"name": "Front-End Developer",
+				"color": "#31252F",
+				"children": [
+					{
+					"name": "Data Visualization Languages",
+					"years": 1,
+					"size": 1,
+					"children": [
+						{"name": "D3.js", "years": 1, "size": 1},
+						{"name": "dc.js", "years": 1, "size": 1},
+						{"name": "crossfilter.js", "years": 1, "size": 1},
+						{"name": "nv.D3", "years": 1, "size": 1},
+						{"name": "Google Charts", "years": 1, "size": 1}
+					]},
+					{
+					"name": "Front-End MVC",
+					"years": 1,
+					"size": 1,
+					"children": [
+						{"name": "angular.js", "years": 1, "size": 1},
+						{"name": "express.js", "years": 1, "size": 1},
+						{"name": "ember.js", "years": 1, "size": 1},
+						{"name": "moustache.js", "years": 1, "size": 1},
+						{"name": "handlebars.js", "years": 1, "size": 1},
+						{"name": "meteor.js", "years": 1, "size": 1},
+						{"name": "node.js", "years": 1, "size": 1},
+						{"name": "MEAN.js", "years": 1, "size": 1},
+						{"name": "Grunt", "years": 1, "size": 1},
+						{"name": "Bower", "years": 1, "size": 1}
+					]},
+					{
+					"name": "jQuery",
+					"years": 1,
+					"size": 1,
+					"children": [
+						{"name": "jQuery UI", "years": 1, "size": 1},
+						{"name": "jQuery Mobile", "years": 1, "size": 1}
+					]
+					},
+					{
+					"name": "Content Management Systems(CMS)",
+					"years": 1,
+					"size": 1,
+					"children": [
+						{"name": "WordPress", "years": 1, "size": 1},
+						{"name": "Joomla", "years": 1, "size": 1},
+						{"name": "phpBB", "years": 1, "size": 1}
+					]
+					},
+					{
+					"name": "Data Formats",
+					"years": 1,
+					"size": 1,
+					"children": [
+						{"name": "CSV", "years": 1, "size": 1},
+						{"name": "TSV", "years": 1, "size": 1},
+						{"name": "JSON", "years": 1, "size": 1},
+						{"name": "BSON", "years": 1, "size": 1},
+						{"name": "XML", "years": 1, "size": 1}
+					]
+					},
+					{"name": "Bootstrap", "years": 1, "size": 1},
+					{"name": "AJAX", "years": 1, "size": 1}
+				]
+				},
+				{
+				"name": "Back-End Developer",
+				"color": "#767281",
+				"children": [
+					{"name": "PHP", "years": 1, "size": 1},
+					{"name": "Python", "years": 1, "size": 1},
+					{
+					"name": "Groovy and Grails(GGTS)",
+					"years": 1,
+					"size": 1,
+					"children": [
+						{"name": "Groovy", "years": 1, "size": 1},
+						{"name": "Grails", "years": 1, "size": 1},
+						{"name": "GSP", "years": 1, "size": 1},
+						{"name": "Gradle", "years": 1, "size": 1},
+						{"name": "Spock", "years": 1, "size": 1}
+					]
+					},
+					{
+					"name": "Eclipse, J2EE",
+					"years": 1,
+					"size": 1,
+					"children": [
+						{"name": "Java", "years": 1, "size": 1},
+						{"name": "JSP", "years": 1, "size": 1},
+						{"name": "JUnit", "years": 1, "size": 1},
+						{"name": "Maven", "years": 1, "size": 1},
+						{"name": "Ant", "years": 1, "size": 1}
+					]
+					},
+					{
+					"name": "Spring(STS)",
+					"years": 1,
+					"size": 1,
+					"children": [
+						{"name": "Spring Framework", "years": 1, "size": 1},
+						{"name": "Spring MVC", "years": 1, "size": 1},
+						{"name": "Spring Security", "years": 1, "size": 1},
+						{"name": "SpringSource TC Server", "years": 1, "size": 1},
+						{"name": "Thymeleaf", "years": 1, "size": 1}
+					]
+					},
+					{
+					"name": "Database Languages",
+					"years": 1,
+					"size": 1,
+					"children": [
+						{"name": "MySQL", "years": 1, "size": 1},
+						{"name": "Oracle", "years": 1, "size": 1},
+						{"name": "HQL", "years": 1, "size": 1},
+						{"name": "SQL", "years": 1, "size": 1},
+						{"name": "Hibernate", "years": 1, "size": 1},
+						{"name": "Mongo DB", "years": 1, "size": 1}
+					]
+					}
+				]
+				}
+			]
+			},
+			{
+			"name": "Engineer",
+			"color": "#775923",
+			"children": [
+				{"name": "Critical Thinking", "size": 1},
+				{"name": "Decision Making", "size": 1},
+				{"name": "Creativity", "size": 1},
+				{"name": "Time Management", "size": 1},
+				{"name": "Active Listenting", "size": 1}
+			]
+			},
+			{
+			"name": "Natural Scientist",
+			"color": "#296A52",
+			"children": [
+				{"name": "Observing", "size": 1},
+				{"name": "Documenting", "size": 1},
+				{"name": "Sharing", "size": 1},
+				{"name": "Critical Thinking", "size": 1},
+				{"name": "Botany", "size": 1},
+				{"name": "Scientific Method", "size": 1}
+			]
+			},
+			{
+			"name": "Social Scientist",
+			"color": "#262561",
+			"children": [
+				{
+				"name": "Journalist",
+				"color": "#412F6B",
+				"children": [
+					{"name": "Blogging", "size": 1},
+					{"name": "Vlogging", "size": 1},
+					{"name": "Interviewing", "size": 1},
+					{"name": "Writing", "size": 1},
+					{"name": "Documenting", "size": 1},
+					{"name": "Researching", "size": 1},
+					{"name": "Organization", "size": 1},
+					{"name": "Planning", "size": 1}
+					]
+				},
+				{
+				"name": "World Historian",
+				"color": "#181737",
+				"children": [
+					{"name": "Analyzing", "size": 1},
+					{"name": "Researching", "size": 1},
+					{"name": "Writing", "size": 1}
+					]
+				},
+				{
+				"name": "Political Economist",
+				"color": "#0C0B11",
+				"children": [
+					{"name": "Researching",
+					"children": [
+						{"name": "Analyzing", "size": 1},
+						{"name": "Researching", "size": 1},
+						{"name": "Writing", "size": 1}
+						]
+					},
+					{"name": "Grant Writing", "size": 1},
+					{"name": "Advocating", "size": 1}
+					]
+				},
+				{
+				"name": "Cultural Anthropologist",
+				"color": "#171449",
+				"children": [
+					{"name": "Analyzing", "size": 1},
+					{"name": "Researching", "size": 1},
+					{"name": "Writing", "size": 1}
+					]
+				},
+				{
+				"name": "Comparative Institutionalist",
+				"color": "#3C5F7D",
+				"children": [
+					{"name": "Analyzing", "size": 1},
+					{"name": "Researching", "size": 1},
+					{"name": "Writing", "size": 1}
+					]
+				},
+				{
+				"name": "Social Activist",
+				"color": "#44336A",
+				"children": [
+					{
+					"name": "Campaigning",
+					"children": [
+						{"name": "Diplomacy", "size": 1},
+						{"name": "Networking", "size": 1},
+						{"name": "Negotiating", "size": 1},
+						{"name": "Haggling", "size": 1},
+						{"name": "Negotiating", "size": 1},
+						{"name": "Debating", "size": 1}
+					]
+					},
+					{
+					"name": "Volunteering",
+						"children": [
+						{
+						"name": "Advocating",
+						"children": [
+							{"name": "Supporting", "size": 1},
+							{"name": "Mediating", "size": 1},
+							{"name": "Preserving", "size": 1},
+							{"name": "Negotiating", "size": 1}
+						]
+						},
+						{"name": "Persevering", "size": 1},
+						{"name": "Persuasion", "size": 1}
+					]
+					}
+				]
+				}
+			]
+			}
+
+	]
+}
+
+
+
+var sunWidth = 840,
+    sunHeight = 370,
+    radius = Math.min(sunWidth, sunHeight) / 2;
+
+var x = d3.scale.linear()
+    .range([0, 2 * Math.PI]);
+
+var y = d3.scale.sqrt()
+    .range([0, radius]);
+
+var color = d3.scale.category20c();
+
+var svg = d3.select("#navSunBurst").append("svg")
+    .attr("width", sunWidth)
+    .attr("height", sunHeight + 10)
+  .append("g")
+    .attr("transform", "translate(" + sunWidth / 2 + "," + (sunHeight / 2 + 10) + ")");
+
+var partition = d3.layout.partition()
+    .value(function(d) { return d.size; });
+
+var arc = d3.svg.arc()
+    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
+
+  svg.call(tipSun);
+
+  var path = svg.selectAll("path")
+      .data(partition.nodes(root))
+    .enter().append("path")
+      .attr("d", arc)
+	  .attr("stroke", "black")
+	  .style("opacity", function(d) {
+		  if(d.depth > 1){
+		  	return 1/(d.depth-1);
+		  }
+		  else {
+		  	return 1;
+		  }
+	  })
+	  .style("fill", function(d) {
+	  if(d.color != null){
+		  return d.color;
+		  } else if(d.parent != null) {
+			  return d.parent.color ? d.parent.color: d.parent.parent.color;
+			  } else {
+			  return color((d.children ? d : d.parent).name);
+			  }
+	  })
+      .on("click", click)
+	  .on("mouseenter", function(d) {
+		  d3.select(this).style("fill", "darkGray");
+		  })
+	  .on("mouseover", tipSun.show)
+	  .on("mouseleave", function(d) {
+		  d3.select(this)
+		  .style("fill", function(d) {
+		  if(d.color != null){
+			  return d.color;
+			  } else if(d.parent != null) {
+				  return d.parent.color ? d.parent.color: d.parent.parent.color;
+				  } else {
+				  return color((d.children ? d : d.parent).name);
+				  }
+		  	  })
+		  })
+	  ;
+
+  function click(d) {
+    path.transition()
+      .duration(750)
+      .attrTween("d", arcTween(d));
+	  d3.selectAll(".centerText")
+	  .transition().duration(400)
+	  .style("opacity", 0)
+	  .style("font-size", "0px");
+	  if(d.name == "Nathaniel Buechler"){
+	  	d3.selectAll(".centerText")
+		.style("display", "")
+		.transition().duration(400)
+		.style("opacity", 1)
+		.style("font-size", "13px");
+	  }
+  }
+
+
+d3.select(self.frameElement).style("height", sunHeight + "px");
+
+// Interpolate the scales!
+function arcTween(d) {
+  var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+      yd = d3.interpolate(y.domain(), [d.y, 1]),
+      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+  return function(d, i) {
+    return i
+        ? function(t) { return arc(d); }
+        : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+  };
+}
+
+svg.append("text")
+		.attr("class", "centerText")
+        .attr("x", (width / width))
+        .attr("y", -10 - (margin.top / margin.top))
+        .attr("text-anchor", "middle")
+        .style("font-size", "13px")
+        .text("Roles and Skills");
+
+svg.append("text")
+		.attr("class", "centerText")
+        .attr("x", (width / width))
+        .attr("y", 10 - (margin.top / margin.top))
+        .attr("text-anchor", "middle")
+        .style("font-size", "13px")
+        .text("Distributed by");
+
+svg.append("text")
+		.attr("class", "centerText")
+        .attr("x", (width / width))
+        .attr("y", 30 - (margin.top / margin.top))
+        .attr("text-anchor", "middle")
+        .style("font-size", "13px")
+        .text("Subject Area");
+
+svg.append("text")
+		.attr("class", "centerText")
+        .attr("x", -(width / 3))
+        .attr("y", 10 - (margin.top / margin.top))
+        .attr("text-anchor", "middle")
+        .style("font-size", "13px")
+        .text("I've explored a few subjects.");
+
+svg.append("text")
+		.attr("class", "centerText")
+        .attr("x", -(width / 3))
+        .attr("y", 30 - (margin.top / margin.top))
+        .attr("text-anchor", "middle")
+        .style("font-size", "13px")
+        .text("(Click an area to zoom)");
