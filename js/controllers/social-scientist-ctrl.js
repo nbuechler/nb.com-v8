e5f50b6364c207fd986e9dc4fa3d151be25c63d5
nbuechler <natebuechler@gmail.com>
2016-04-14 23:34:49 -0400

    added a controller

---
+++ js/controllers/social-scientist-ctrl.js
@@ -0,0 +1,6 @@
+// NOTE: routerApp is defined in app.js
+routerApp.controller('socialController', ['$scope', '$http',
+    function($scope, $http) {
+      $scope.default = 'Projects are listed on the left. Select one to read more about the topic.';
+
+    }]);
