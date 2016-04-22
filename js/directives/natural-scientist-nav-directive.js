4451b984a964d9714c117580501447edc5655f55
nbuechler <natebuechler@gmail.com>
2016-04-21 21:27:40 -0400

    added nav directives

---
+++ js/directives/natural-scientist-nav-directive.js
@@ -0,0 +1,7 @@
+// NOTE: routerApp is defined in app.js
+routerApp.directive("naturalScientistNavDirective", function() {
+    return {
+        restrict : 'E',
+        templateUrl : 'js/templates/natural-scientist-nav.html'
+    };
+});
---
+++ js/directives/social-scientist-nav-directive.js
@@ -0,0 +1,7 @@
+// NOTE: routerApp is defined in app.js
+routerApp.directive("socialScientistNavDirective", function() {
+    return {
+        restrict : 'E',
+        templateUrl : 'js/templates/social-scientist-nav.html'
+    };
+});
