31292f5e8a3682cd3489770c2c3e5b37abafbfb2
nbuechler <natebuechler@gmail.com>
2016-04-28 00:07:17 -0400

    added artist containers

--- js/directives/image-container-directive.js
+++ js/directives/image-container-directive.js
@@ -1,11 +1,11 @@
 // NOTE: routerApp is defined in app.js
-routerApp.directive("imageContainerArtistDirective", function() {
+routerApp.directive("imageArtistContainerDirective", function() {
     return {
         restrict : 'E',
         templateUrl : 'js/templates/image-artist-container.html'
     };
 });
-routerApp.directive("imageContainerSocialDirective", function() {
+routerApp.directive("imageSocialContainerDirective", function() {
     return {
         restrict : 'E',
         templateUrl : 'js/templates/image-social-container.html'
