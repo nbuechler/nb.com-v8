d99c331aabcdf0783cb0842968d86d59a96c9fd1
nbuechler <natebuechler@gmail.com>
2016-05-07 01:36:58 -0400

    added affective engineering contnt

--- js/directives/article-container-directive.js
+++ js/directives/article-container-directive.js
@@ -5,3 +5,10 @@ nbApp.directive("articleContainerDirective", function() {
         templateUrl : 'js/templates/article-container.html'
     };
 });
+// NOTE: this is used for engineering
+nbApp.directive("engineerArticleContainerDirective", function() {
+    return {
+        restrict : 'E',
+        templateUrl : 'js/templates/engineer-article-container.html'
+    };
+});
