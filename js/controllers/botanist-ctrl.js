684513aecc9ee041e9c9f793752a3683b347d612
nbuechler <natebuechler@gmail.com>
2016-05-15 23:17:53 -0400

    botanist pictures

--- js/controllers/botanist-ctrl.js
+++ js/controllers/botanist-ctrl.js
@@ -1,6 +1,10 @@
 // NOTE: nbApp is defined in app.js
 nbApp.controller('botanistController', ['$scope', '$http',
     function($scope, $http) {
+      $scope.default = 'Images are shown below. Select one to see the image.';
+      $scope.selectedProject = null;
+      $scope.title = 'Botanist Projects';
+
       // {"name": "", "filepath": "", "t_filepath": ""},
       var allProjects = [
       ];
@@ -9,6 +13,28 @@ nbApp.controller('botanistController', ['$scope', '$http',
 
       $scope.selectProject = function(improjectage) {
           $scope.selectProject = project;
+      };
+
+      var rootPath = '../content/roles/botanist/WR/';
+      var tRootPath = '../content/roles/botanist/T/';
+      // {"name": "", "filepath": "", "t_filepath": ""},
+      var botanistImages = [
+        {"name": "", "filepath": rootPath + "01.jpg", "t_filepath": tRootPath + "01-T.jpg"},
+        {"name": "", "filepath": rootPath + "02.jpg", "t_filepath": tRootPath + "02-T.jpg"},
+        {"name": "", "filepath": rootPath + "03.jpg", "t_filepath": tRootPath + "03-T.jpg"},
+        {"name": "", "filepath": rootPath + "04.jpg", "t_filepath": tRootPath + "04-T.jpg"},
+        {"name": "", "filepath": rootPath + "05.jpg", "t_filepath": tRootPath + "05-T.jpg"},
+        {"name": "", "filepath": rootPath + "06.jpg", "t_filepath": tRootPath + "06-T.jpg"},
+        {"name": "", "filepath": rootPath + "07.jpg", "t_filepath": tRootPath + "07-T.jpg"},
+        {"name": "", "filepath": rootPath + "08.jpg", "t_filepath": tRootPath + "08-T.jpg"},
+        {"name": "", "filepath": rootPath + "09.jpg", "t_filepath": tRootPath + "09-T.jpg"},
+        {"name": "", "filepath": rootPath + "10.jpg", "t_filepath": tRootPath + "10-T.jpg"},
+      ]
+
+      $scope.images = botanistImages;
+
+      $scope.selectImage = function(image) {
+          $scope.selectedImage = image;
       };
 
       /*
